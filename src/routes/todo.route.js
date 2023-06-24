2f9c23a6fe046ac0c9105596732ef1e8b8ff0b21
Farrukh Zeb <FarrukhXeb@gmail.com>
2023-06-25 00:00:29 +0500

    refactor: using module.exports for controller

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const validate = require('../middlewares/validate');
 const todoValidation = require('../validations/todo.validation');
-const todoController = require('../controllers/todo.controller');
+const { todoController } = require('../controllers');
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
